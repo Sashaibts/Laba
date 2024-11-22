2ac973cd5d3cfc53111f0efaac6cf1d972217dcd
Grigory Savin <36847935+Komodood@users.noreply.github.com>
2024-11-23 00:42:43 +0500

    Заготовка

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
+void readFile(vector <string> Myvector)
+{
 
+}
+void print(vector <string> Myvector)
+{
+
+}
+void writeFile(vector <string> Myvector)
+{
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector <string> Myvector;
+    readFile(Myvector);
+    print(Myvector);
+    writeFile(Myvector);
     return 0;
 }
